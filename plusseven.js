44343ae4381da41baee96e8d2a6fbe052a8ce8a0
Дмитрий Кузьмин < >
2016-04-20 04:36:36 +0600

    Experimental realtime gameplay

--- plusseven.js
+++ plusseven.js
@@ -14,6 +14,8 @@ var o = {
     'y': 5,
     'value': size
 };
+var cooldown = 2000;
+var cd_modifier = 0.2;
 // array for storing field values
 var items = new Array(size);
 // preinitialising it
@@ -23,18 +25,20 @@ for (var i = 0; i < size; i++) {
 // block generates op randomly
 var ops = ["+", "-", "*", "/"];
 // chances of appering op in block
-var op_chances = [0.4, 0.4, 0.15, 0.05];
+var op_chances = [0.3, 0.3, 0.25, 0.15];
+var op_lives = [3, 3, 1, 1];
 var op_styles = {
-    "+" : "op_add",
-    "-" : "op_sub", 
-    "*" : "op_mul",
-    "/" : "op_div"
+    "+": "op_add",
+    "-": "op_sub",
+    "*": "op_mul",
+    "/": "op_div"
 };
 // value, which you must reach for win
-var finish = Math.floor(Math.random() * 1000);
-var lose_max = 9999;
+var finish = 100 + Math.floor(Math.random() * 800);
+var lose_max = 999;
 // number of player steps on field
 var steps = 0;
+
 
 
 // filling field randomly
@@ -44,17 +48,18 @@ for (var i = 0; i < size; i++)
     }
 
 function reborn_block(i, j) {
-    
+
     var lucky;
+    var r = Math.random();
     for (lucky = 0; lucky < ops.length - 1; lucky++)
-    if (Math.random() <= op_chances[lucky]) break;
-    console.log(lucky);
-    var sign_index = lucky;//Math.floor(Math.random() * ops.length);
+        if (r <= op_chances[lucky]) break;
+        else r -= op_chances[lucky];
+    var sign_index = lucky;
     var item = {
         'sign': ops[sign_index],
-        'number': 1 + Math.floor(Math.random() * (size-1)),
+        'number': 1 + Math.floor(Math.random() * (size - 1)),
         'locked': false,
-        'lives': 3
+        'lives': op_lives[sign_index]
     };
     items[i][j] = item;
 }
@@ -115,7 +120,12 @@ function isOutOfBounds(pos) {
 		if 	( !Number.isInteger(result) ||
 					  result < 0) 
 	}*/
+
+var intervalID;
+var modified_cooldown = cooldown;
+
 function checkKey(e) {
+
 
     e = e || window.event;
     var oNext = Object.assign({}, o);
@@ -131,9 +141,10 @@ function checkKey(e) {
     } else if (e.keyCode == '39') {
         // right arrow
         oNext.y++;
-    }
-    else return;
-    if (items[oNext.y][oNext.x].locked == false && !isOutOfBounds(oNext)) {
+    } else return;
+
+
+    if (!isOutOfBounds(oNext) && items[oNext.y][oNext.x].locked == false) {
         var oOld = Object.assign({}, o);
         o = oNext;
         o.value = step(o.value, items[o.y][o.x]);
@@ -143,18 +154,61 @@ function checkKey(e) {
         item.lives--;
         if (item.lives == 0) reborn_block(oOld.y, oOld.x);
         steps++;
+        update();
     }
+    else  { if (o.value > finish) o.value+= 1 + Math.floor(Math.random() * 20);
+            else if (o.value < finish) o.value-= + Math.floor(Math.random() * 20); update();}
     //else console.log("STOP");
-    document.getElementById("canvas").innerHTML = updateHTML();
-    document.getElementById("steps").innerHTML = steps;
-    if (!checkFinish())
-        checkLose();
+
+
+    checkFinish()
+}
+
+function update()
+{
+        clearInterval(intervalID);
+        if (!checkFinish())
+                checkLose();
+        document.getElementById("canvas").innerHTML = updateHTML();
+        document.getElementById("steps").innerHTML = steps;
+
+        modified_cooldown = cooldown * (1.0 - (500.0 - Math.min(Math.abs(o.value - finish), 500.0)) * (1.0 - cd_modifier) / 500.0);
+
+        var bar = new ProgressBar.Path('#path', {
+            easing: 'easeInOut',
+            duration: modified_cooldown
+        });
+        bar.set(0);
+        bar.animate(1.0);
+        intervalID = setInterval(function() {
+            if (o.value > finish) o.value++;
+            else if (o.value < finish) o.value--;
+            //clearInterval(intervalID);
+
+            lock(o.value);
+            modified_cooldown = cooldown * (1.0 - (500.0 - Math.min(Math.abs(o.value - finish), 500.0)) * (1.0 - cd_modifier) / 500.0);
+
+            document.getElementById("canvas").innerHTML = updateHTML();
+
+            bar = null;
+            bar = new ProgressBar.Path('#path', {
+                easing: 'easeInOut',
+                duration: modified_cooldown
+            });
+            bar.set(0);
+            bar.animate(1.0);
+            //bar.animate(1.0);
+
+            
+
+        }, modified_cooldown);
 }
 
 function checkFinish() {
     if (finish == o.value) {
         document.getElementById("win").innerHTML = "<h1>YOU WIN!</h1><a href='index.html'><center>restart</center></a>";
         document.onkeydown = null;
+        clearInterval(intervalID);
         return true;
     } else return false;
 }
@@ -164,10 +218,11 @@ function checkLose() {
         lockedOrNotExists(o.y - 1, o.x) &&
         lockedOrNotExists(o.y, o.x + 1) &&
         lockedOrNotExists(o.y, o.x - 1) ||
-        o.value > lose_max) {
+        o.value > lose_max || o.value < 1) {
         document.getElementById("win").innerHTML = "<h1>YOU LOSE!</h1><a href='index.html'><center>restart</center></a>";
         document.onkeydown = null;
         document.getElementById("canvas").className += " blur";
+        clearInterval(intervalID);
         return true;
     } else return false;
 }
@@ -178,9 +233,9 @@ function lockedOrNotExists(y, x) {
     }))
         return true;
     else
-    if (items[y][x].locked)
-        return true;
-    return false;
+    /*if (items[y][x].locked)
+        return true;*/
+        return false;
 }
 
 function updateHTML() {
@@ -190,17 +245,15 @@ function updateHTML() {
         for (var j = 0; j < size; j++) {
             c += "<td>";
             if (o.x == i && o.y == j) {
-                c += "<div class='block selected'>";
-                c += o.value;
+                c += "<div id='cooldown'>"; //class='block selected'>";
+                c += "<svg style='z-index:1;'xmlns='http://www.w3.org/2000/svg' version='1.1' x='0px' y='0px' viewBox='0 0 40 40'><path id='path' fill-opacity='0' stroke-width='8' stroke='#FF9900' d='M20,0 40,0 40,40 0,40 0,0 20,0'/></svg>"
+                c += "<div class='number'>" + o.value + "</div>";
             } else {
                 var locked = items[j][i].locked ? "locked" : "";
-                var lives = "lives"+ items[j][i].lives;
+                var lives = "lives" + items[j][i].lives;
                 var opstyle = op_styles[items[j][i].sign];
                 //switch ()
-                c += "<div class='block " 
-                + locked + " " 
-                + lives + " "
-                + opstyle + "'>";
+                c += "<div class='block " + locked + " " + lives + " " + opstyle + "'>";
                 c += items[j][i].sign + items[j][i].number;
             }
             c += "</div>";
@@ -213,5 +266,5 @@ function updateHTML() {
 }
 
 function gen_seedlink() {
-    document.getElementById("seeddiv").innerHTML =  "xotonic.github.io/?" + seed;
+    document.getElementById("seeddiv").innerHTML = "xotonic.github.io/?" + seed;
 }
